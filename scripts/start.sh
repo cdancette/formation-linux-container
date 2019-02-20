03256b36778a24d6efe57462287bd4243686335f
Corentin <corentin@cdancette.fr>
2019-02-21 00:55:21 +0100

    comment useless stuff

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,36 +20,35 @@ chmod -R 777 /dev/pts/
 
 #mysql -pwololo < /home/level5/createUser.sql
 #mysql -pwololo < /home/level5/dump.sql
-
-rm -f /home/level5/dump.sql
-rm -f /home/level5/createUser.sql
+# rm -f /home/level5/dump.sql
+# rm -f /home/level5/createUser.sql
 
 # Change password for level1_5 according to kernel version
-uname -r > /usr/local/passwd
-rm -f /usr/local/passwd
+# uname -r > /usr/local/passwd
+# rm -f /usr/local/passwd
 
 # Change password for level10 according to PIDs
-ps aux | grep cron | grep -v grep | awk -F' ' '{ print $2 }' > /usr/local/passwd1
-ps aux | grep mysqld_safe | grep -v grep | awk -F' ' '{ print $2 }' >> /usr/local/passwd1
-echo 'bash' >> /usr/local/passwd1
+# ps aux | grep cron | grep -v grep | awk -F' ' '{ print $2 }' > /usr/local/passwd1
+# ps aux | grep mysqld_safe | grep -v grep | awk -F' ' '{ print $2 }' >> /usr/local/passwd1
+# echo 'bash' >> /usr/local/passwd1
 
-sed ':a;N;$!ba;s/\n//g' /usr/local/passwd1 > /usr/local/passwd2
+# sed ':a;N;$!ba;s/\n//g' /usr/local/passwd1 > /usr/local/passwd2
 
-chmod 400 /usr/local/passwd2
+# chmod 400 /usr/local/passwd2
 
-cat /usr/local/passwd2 /usr/local/passwd2 | passwd level10
+# cat /usr/local/passwd2 /usr/local/passwd2 | passwd level10
 
-rm -f /usr/local/passwd1
-rm -f /usr/local/passwd2
+# rm -f /usr/local/passwd1
+# rm -f /usr/local/passwd2
 
 # Use nc and change password for level19 according to port used
-/bin/nc -k -l 8080 | bash &
-netstat -tulpn | grep nc | awk -F' ' '{ print $4 }' | awk -F':' '{ print $2 }' > /usr/local/passwd3
+#/bin/nc -k -l 8080 | bash &
+#netstat -tulpn | grep nc | awk -F' ' '{ print $4 }' | awk -F':' '{ print $2 }' > /usr/local/passwd3
 
-cat /usr/local/passwd3 /usr/local/passwd3 | passwd level19
+# cat /usr/local/passwd3 /usr/local/passwd3 | passwd level19
 
-chmod 400 /usr/local/passwd3
-rm -f /usr/local/passwd3
+# chmod 400 /usr/local/passwd3
+# rm -f /usr/local/passwd3
 
 # Populate postgresql database for level19
 # service postgresql start
